477aa3b5955c3e2b441eae8ec8655b3e7f8f7ef0
SNAcorp <snagithub@gmail.com>
2025-02-01 18:55:39 +0800

    Add negative adapters

--- pkg/pca9685/adapter_not_d2r2_linux.go
+++ pkg/pca9685/adapter_not_d2r2_linux.go
@@ -1,5 +1,5 @@
-//go:build !linux
-// +build !linux
+//go:build windows || darwin
+// +build windows darwin
 
 package pca9685
 
@@ -7,7 +7,7 @@ import (
 	"fmt"
 )
 
-// NewI2CAdapterD2r2 выводит сообщение об ошибке и возвращает тестовый адаптер.
+// NewI2CAdapterD2r2 выводит сообщение об ошибке и возвращает ошибку.
 func NewI2CAdapterD2r2() error {
 	return fmt.Errorf("ПРЕДУПРЕЖДЕНИЕ: адаптер d2r2/go-i2c работает только на Linux. Используйте тестовый адаптер для вашей системы.")
 }
--- pkg/pca9685/adapter_not_periph_io_linux.go
+++ pkg/pca9685/adapter_not_periph_io_linux.go
@@ -1,9 +1,9 @@
-//go:build !linux
-// +build !linux
+//go:build windows || darwin
+// +build windows darwin
 
 package pca9685
 
-// NewI2CAdapterPeriph выводит сообщение об ошибке и возвращает тестовый адаптер.
+// NewI2CAdapterPeriph выводит сообщение об ошибке и возвращает ошибку.
 func NewI2CAdapterPeriph() error {
 	return fmt.Errorf("ПРЕДУПРЕЖДЕНИЕ: адаптер periph.io/go-i2c работает только на Linux. Используйте тестовый адаптер для вашей системы.")
 }
