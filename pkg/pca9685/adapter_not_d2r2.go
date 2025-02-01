f1a007e4008c1f02fed753aa9ddfb24193fa742b
SNAcorp <snagithub@gmail.com>
2025-02-01 19:03:52 +0800

    Rename

--- pkg/pca9685/adapter_not_d2r2.go
+++ pkg/pca9685/adapter_not_d2r2.go
@@ -1,4 +1,4 @@
-//go:build windows || darwin
+//go:build !linux
 
 package pca9685
 
@@ -6,7 +6,7 @@ import (
 	"fmt"
 )
 
-// NewI2CAdapterD2r2 выводит сообщение об ошибке и возвращает ошибку.
+// ПРЕДУПРЕЖДЕНИЕ: адаптер d2r2/go-i2c работает только на Linux. Используйте тестовый адаптер для вашей системы.
 func NewI2CAdapterD2r2() error {
 	return fmt.Errorf("ПРЕДУПРЕЖДЕНИЕ: адаптер d2r2/go-i2c работает только на Linux. Используйте тестовый адаптер для вашей системы.")
 }
--- pkg/pca9685/adapter_not_periph_io.go
+++ pkg/pca9685/adapter_not_periph_io.go
@@ -1,8 +1,10 @@
-//go:build windows || darwin
+//go:build !linux
 
 package pca9685
 
-// NewI2CAdapterPeriph выводит сообщение об ошибке и возвращает ошибку.
+import "fmt"
+
+// ПРЕДУПРЕЖДЕНИЕ: адаптер periph.io/go-i2c работает только на Linux. Используйте тестовый адаптер для вашей системы.
 func NewI2CAdapterPeriph() error {
 	return fmt.Errorf("ПРЕДУПРЕЖДЕНИЕ: адаптер periph.io/go-i2c работает только на Linux. Используйте тестовый адаптер для вашей системы.")
 }
